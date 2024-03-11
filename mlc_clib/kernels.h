09a99eab7314e61ff60429fff7ac84d22f4b7c9a
rebcabin <rebcabin@users.noreply.github.com>
2024-03-10 21:26:35 -0700

    initial memory management

--- mlc_clib/kernels.h
+++ mlc_clib/kernels.h
@@ -5,6 +5,69 @@
 #ifndef MLC_CLIB_KERNELS_H
 #define MLC_CLIB_KERNELS_H
 
+
 void test_linkage();
+
+/*
+ * For the specs of all operations, see the file
+ * "test_beautiful.mnist.specs.txt".
+ */
+
+
+
+/*
+ * All the type=<Type...> specs are for f32s, so that's the
+ * only type we need here.
+ */
+
+
+typedef float f32;
+
+
+/*
+ * There are arrays of rank 1, 2, and 3 in the spec.
+ */
+
+
+typedef struct array_1 {
+    f32 * p_storage;
+    int dim;
+} A1, * pA1;
+
+
+pA1 A1_alloc(int dim);
+/*
+ * Return a null pointer to help calling code to avoid using
+ * deallocated memory.
+ */
+pA1 A1_free(pA1 it);
+void A1_validate(pA1 it);
+void A1_dump(pA1 it);
+
+
+/*
+ * The innermost operation is a Conv2D with the following spec:
+ *
+ * (Operation(
+ *     op_type=<OpType.Conv2D: 6>,
+ *     args=(
+ *         Array(
+ *             name='conv_kernel1',
+ *             type=<Type.f32: 3>,
+ *             rank=2,
+ *             shape=(5, 5, 1, 32),
+ *             memory_space=<MemorySpace.host: 1>),
+ *         Array(
+ *             name='Input',
+ *             type=<Type.f32: 3>,
+ *             rank=2,
+ *             shape=(28, 28),
+ *             memory_space=<MemorySpace.host: 1>)),
+ *     execution_space=<ExecutionSpace.host: 1>,
+ *     type=<Type.f32: 3>,
+ *     rank=3,
+ *     shape=[24, 24, 32],
+ *     memory_space=<MemorySpace.host: 1>),)
+ */
 
 #endif //MLC_CLIB_KERNELS_H
--- mlc_clib/main.c
+++ mlc_clib/main.c
@@ -2,7 +2,21 @@
 
 #include "kernels.h"
 
+/*
+ * On Mac, run "leaks" on the executable.
+ *
+ * leaks --atExit -- ./mlc_clib
+ */
+
 int main() {
     test_linkage();
+    pA1 pa1 = A1_alloc(42);
+    for (int i = 0; i < 42; i++) {
+        pa1->p_storage[i] = ((f32)(i));
+    }
+    A1_dump(pa1);
+    A1_free(pa1);
+    // DON'T DO THIS!
+    // A1_dump(pa1);
     return 0;
 }
