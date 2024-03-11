a385ff6fc66c2d24421eedb78f62bc4e71b9472f
rebcabin <rebcabin@users.noreply.github.com>
2024-03-10 19:50:03 -0700

    skeleton for C code

---
+++ mlc_clib/kernels.h
@@ -0,0 +1,10 @@
+//
+// Created by Brian Beckman on 3/10/24.
+//
+
+#ifndef MLC_CLIB_KERNELS_H
+#define MLC_CLIB_KERNELS_H
+
+void test_linkage();
+
+#endif //MLC_CLIB_KERNELS_H
---
+++ mlc_clib/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+#include "kernels.h"
+
+int main() {
+    test_linkage();
+    return 0;
+}
