ac099bd321313d39eb5a0e7fb6e16e30607bbaa8
rebcabin <rebcabin@users.noreply.github.com>
2024-03-11 08:51:22 -0700

    4D mem mgt

--- mlc_clib/kernels.h
+++ mlc_clib/kernels.h
@@ -25,7 +25,7 @@ typedef float f32;
 
 
 /*
- * There are arrays of rank 1, 2, and 3 in the spec.
+ * There are arrays of rank 1, 2, 3, and 4 in the spec.
  *
  * Internally, to start, let's have a non-contiguous storage
  * model, where a 2-D array is an array of pointers to 1-D arrays,
@@ -35,13 +35,19 @@ typedef float f32;
  */
 
 
+/*                _       _  */
+/*  _ _ __ _ _ _ | |_____/ | */
+/* | '_/ _` | ' \| / /___| | */
+/* |_| \__,_|_||_|_\_\   |_| */
+
+
 typedef struct array_1 {
     f32 * p_storage;
-    int dim;
+    int cols;
 } A1, * pA1;
 
 
-pA1 A1_alloc(int dim);
+pA1 A1_alloc(int cols);
 /*
  * Return a null pointer from "free" to help calling code to avoid
  * using deallocated memory.
@@ -49,6 +55,12 @@ pA1 A1_alloc(int dim);
 pA1 A1_free(pA1 it);
 void A1_validate(pA1 it);
 void A1_dump(pA1 it);
+
+
+/*                _      ___  */
+/*  _ _ __ _ _ _ | |____|_  ) */
+/* | '_/ _` | ' \| / /___/ /  */
+/* |_| \__,_|_||_|_\_\  /___| */
 
 
 typedef struct array_2 {
@@ -64,6 +76,12 @@ void A2_validate(pA2 it);
 void A2_dump(pA2 it);
 
 
+/*                _      ____ */
+/*  _ _ __ _ _ _ | |____|__ / */
+/* | '_/ _` | ' \| / /___|_ \ */
+/* |_| \__,_|_||_|_\_\  |___/ */
+
+
 typedef struct array_3 {
     f32 *** ppp_storage;
     int rows;
@@ -76,6 +94,27 @@ pA3 A3_alloc(int rows, int cols, int sheets);
 pA3 A3_free(pA3 it);
 void A3_validate(pA3 it);
 void A3_dump(pA3 it);
+
+
+/*                _       _ _   */
+/*  _ _ __ _ _ _ | |_____| | |  */
+/* | '_/ _` | ' \| / /___|_  _| */
+/* |_| \__,_|_||_|_\_\     |_|  */
+
+
+typedef struct array_4 {
+    f32 **** pppp_storage;
+    int rows;
+    int cols;
+    int sheets;
+    int blocks;
+} A4, * pA4;
+
+
+pA4 A4_alloc(int rows, int cols, int sheets, int blocks);
+pA4 A4_free(pA4 it);
+void A4_validate(pA4 it);
+void A4_dump(pA4 it);
 
 
 /*
@@ -102,5 +141,9 @@ void A3_dump(pA3 it);
  *     shape=[24, 24, 32],
  *     memory_space=<MemorySpace.host: 1>),)
  */
+
+
+pA3 Conv2D(pA4 conv_kernel1, pA2 Input);
+
 
 #endif //MLC_CLIB_KERNELS_H
