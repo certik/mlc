aec0442415b2a42baf7dd198f501667f2133c0f7
Ondřej Čertík <ondrej@certik.us>
2024-03-12 14:30:25 -0600

    Header

--- mlc_clib/main.c
+++ mlc_clib/main.c
@@ -18,6 +18,10 @@ void test_4D_MLCA();
  * leaks --atExit -- ./mlc_clib
  */
 
+
+#define GGUF_MAGIC "GGUF"
+#define GGUF_MAX_DIMS           4
+
 struct gguf_str {
     uint64_t n;  // GGUFv2
     char * data;
@@ -39,6 +43,39 @@ enum gguf_type {
     GGUF_TYPE_FLOAT64 = 12,
     GGUF_TYPE_COUNT,       // marks the end of the enum
 };
+
+    enum ggml_type {
+        GGML_TYPE_F32  = 0,
+        GGML_TYPE_F16  = 1,
+        GGML_TYPE_Q4_0 = 2,
+        GGML_TYPE_Q4_1 = 3,
+        // GGML_TYPE_Q4_2 = 4, support has been removed
+        // GGML_TYPE_Q4_3 (5) support has been removed
+        GGML_TYPE_Q5_0 = 6,
+        GGML_TYPE_Q5_1 = 7,
+        GGML_TYPE_Q8_0 = 8,
+        GGML_TYPE_Q8_1 = 9,
+        // k-quantizations
+        GGML_TYPE_Q2_K = 10,
+        GGML_TYPE_Q3_K = 11,
+        GGML_TYPE_Q4_K = 12,
+        GGML_TYPE_Q5_K = 13,
+        GGML_TYPE_Q6_K = 14,
+        GGML_TYPE_Q8_K = 15,
+        GGML_TYPE_IQ2_XXS = 16,
+        GGML_TYPE_IQ2_XS  = 17,
+        GGML_TYPE_IQ3_XXS = 18,
+        GGML_TYPE_IQ1_S   = 19,
+        GGML_TYPE_IQ4_NL  = 20,
+        GGML_TYPE_IQ3_S   = 21,
+        GGML_TYPE_IQ2_S   = 22,
+        GGML_TYPE_IQ4_XS  = 23,
+        GGML_TYPE_I8,
+        GGML_TYPE_I16,
+        GGML_TYPE_I32,
+        GGML_TYPE_COUNT,
+    };
+
 
 union gguf_value {
     uint8_t  uint8;
@@ -92,7 +129,17 @@ struct gguf_context {
     void * data;
 };
 
-#define GGUF_MAGIC "GGUF"
+struct gguf_tensor_info {
+    struct gguf_str name;
+    uint32_t n_dims;
+    uint64_t ne[GGUF_MAX_DIMS];
+    enum ggml_type type;
+    uint64_t offset; // offset from start of `data`, must be a multiple of `ALIGNMENT`
+    // for writing API
+    const void * data;
+    size_t size;
+};
+
 
 static bool gguf_fread_el(FILE * file, void * dst, size_t size,
         size_t * offset)
@@ -128,6 +175,36 @@ int gguf_read(const char *fname, struct gguf_context *ctx)
 
     // Header
     {
+        bool ok = true;
+
+        ctx->kv    = NULL;
+        ctx->infos = NULL;
+        ctx->data  = NULL;
+
+        ok = ok && gguf_fread_el(file, &ctx->header.version,
+                sizeof(ctx->header.version),   &offset);
+        ok = ok && gguf_fread_el(file, &ctx->header.n_tensors,
+                sizeof(ctx->header.n_tensors), &offset);
+        ok = ok && gguf_fread_el(file, &ctx->header.n_kv,
+                sizeof(ctx->header.n_kv),      &offset);
+
+        if (ctx->header.version == 1) {
+            fprintf(stderr, "%s: GGUFv1 is no longer supported. please use a more up-to-date version\n", __func__);
+            fclose(file);
+            return 3;
+        }
+
+        // sanity-checks to prevent from integer/buffer overflows
+        ok = ok && (ctx->header.n_tensors
+                < (SIZE_MAX/2)/sizeof(struct gguf_tensor_info));
+        ok = ok && (ctx->header.n_kv
+                < (SIZE_MAX/2)/sizeof(struct gguf_kv));
+
+        if (!ok) {
+            fprintf(stderr, "%s: failed to read header\n", __func__);
+            fclose(file);
+            return 4;
+        }
     }
     return 0;
 }
@@ -139,6 +216,9 @@ int main() {
         printf("File read successfuly.\n");
         printf("Magic:'%c%c%c%c'\n", ctx.header.magic[0], ctx.header.magic[1],
                 ctx.header.magic[2], ctx.header.magic[3]);
+        printf("Version: %d\n", ctx.header.version);
+        printf("Number of arrays: %llu\n", ctx.header.n_tensors);
+        printf("Number of kv pairs: %llu\n", ctx.header.n_kv);
     }
     return r;
 }
