2de282b5a0e612ad4d91f6315fbef5cda905034a
rebcabin <rebcabin@users.noreply.github.com>
2024-03-11 18:20:59 -0700

    add contig mem, delete sheaved mem

--- mlc_clib/kernels.c
+++ mlc_clib/kernels.c
@@ -8,10 +8,13 @@
 #include <string.h>  // for memset
 #include "kernels.h"
 #include "assert.h"
+#include "uuid/uuid.h"
 
+
+void generate_name(pMLCA result);
 
 void test_linkage() {
-    printf("Hello from kernels.c !");
+    printf("Hello from kernels.c !\n");
 }
 
 
@@ -72,359 +75,293 @@ int64_t blocks(pMLCA pmlca) {
 }
 
 
-void validate(pMLCA pmlca) {
+void validateMLCA(pMLCA pmlca) {
     assert(pmlca != NULL);
     assert(rank(pmlca) >= 1);
     assert(pmlca->data != NULL);
+    // It must have columns.
     assert(cols(pmlca) <= MLC_MAX_COLS);
-    if (pmlca->ne[1] != 0) {
+    assert(cols(pmlca) >= 1);
+    if (pmlca->ne[MLC_ROWS_IDX] != 0) {
         assert(rank(pmlca) >= 2);
+        assert(rows(pmlca) >= 1);
         assert(rows(pmlca) <= MLC_MAX_ROWS);
     } else {
-        assert(pmlca->ne[2] == 0);
-        assert(pmlca->ne[3] == 0);
+        // If it doesn't have rows, it must not have sheets and blocks.
+        assert(pmlca->ne[MLC_SHEETS_IDX] == 0);
+        assert(pmlca->ne[MLC_BLOCKS_IDX] == 0);
     }
-    if (pmlca->ne[2] != 0) {
+    if (pmlca->ne[MLC_SHEETS_IDX] != 0) {
         assert(rank(pmlca) >= 3);
+        assert(sheets(pmlca) >= 1);
         assert(sheets(pmlca) <= MLC_MAX_SHEETS);
     } else {
-        assert(pmlca->ne[3] == 0);
+        // If it doesn't have sheets, it must not have blocks.
+        assert(pmlca->ne[MLC_BLOCKS_IDX] == 0);
     }
     if (pmlca->ne[3] != 0) {
         assert(rank(pmlca) == 4);
+        assert(blocks(pmlca) >= 1);
         assert(blocks(pmlca) <= MLC_MAX_BLOCKS);
     }
+    int64_t total_elts = 1;
+    for (int r = 0; r < rank(pmlca); r++) {
+        total_elts *= pmlca->ne[r];
+    }
+    int64_t total_size = ((int64_t) (total_elts * MLC_DATUM_SIZE));
+    assert(total_size == pmlca->total_size);
 }
 
 
-f32 get1D(pMLCA pmlca, int col) {
-    assert(pmlca != NULL);
+f32 get1D(pMLCA pmlca, int64_t col) {
+    validateMLCA(pmlca);
     assert(rank(pmlca) == 1);
-    assert(pmlca->data != NULL);
-    assert(col < cols(pmlca));
-    f32 result = ((f32 *)(pmlca->data))[col];
+    f32 result = ((f32 *) (pmlca->data))[col];
     return result;
 }
 
 
-void put1D(pMLCA pmlca, int col, f32 val) {
-    assert(pmlca != NULL);
+f32 get2D(pMLCA pmlca, int64_t row, int64_t col) {
+    validateMLCA(pmlca);
+    assert(rank(pmlca) == 2);
+    int64_t index = (row * cols(pmlca)) + col;
+    f32 result = ((f32 *) (pmlca->data))[index];
+    return result;
+}
+
+
+f32 get3D(pMLCA pmlca, int64_t sheet,
+          int64_t row, int64_t col) {
+    validateMLCA(pmlca);
+    assert(rank(pmlca) == 3);
+    int64_t index = (sheet * rows(pmlca) * cols(pmlca))
+                    + (row * cols(pmlca)) + col;
+    f32 result = ((f32 *) (pmlca->data))[index];
+    return result;
+}
+
+
+f32 get4D(pMLCA pmlca,
+        int64_t block, int64_t sheet,
+        int64_t row, int64_t col) {
+    validateMLCA(pmlca);
+    assert(rank(pmlca) == 4);
+    int64_t index =
+            (block * sheets(pmlca) * rows(pmlca) * cols(pmlca))
+            + (sheet * rows(pmlca) * cols(pmlca))
+            + (row * cols(pmlca)) + col;
+    f32 result = ((f32 *) (pmlca->data))[index];
+    return result;
+}
+
+
+void put1D(pMLCA pmlca, int64_t col, f32 val) {
+    validateMLCA(pmlca);
     assert(rank(pmlca) == 1);
-    assert(pmlca->data != NULL);
-    assert(col < cols(pmlca));
-    f32 * pointer  = ((f32 *)(pmlca->data));
+    f32 *pointer = ((f32 *) (pmlca->data));
     pointer[col] = val;
 }
 
 
-//pMLCA alloc1D
+void put2D(pMLCA pmlca, int64_t row, int64_t col, f32 val) {
+    validateMLCA(pmlca);
+    assert(rank(pmlca) == 2);
+    f32 *pointer = ((f32 *) (pmlca->data));
+    int64_t index = (row * cols(pmlca)) + col;
+    pointer[index] = val;
+}
 
 
-/*  ____  _   _ _____    ___     _______ ____   */
-/* / ___|| | | | ____|  / \ \   / / ____|  _ \  */
-/* \___ \| |_| |  _|   / _ \ \ / /|  _| | | | | */
-/*  ___) |  _  | |___ / ___ \ V / | |___| |_| | */
-/* |____/|_| |_|_____/_/   \_\_/  |_____|____/  */
+void put3D(pMLCA pmlca, int64_t sheet,
+           int64_t row, int64_t col, f32 val) {
+    validateMLCA(pmlca);
+    assert(rank(pmlca) == 3);
+    f32 *pointer = ((f32 *) (pmlca->data));
+    int64_t index = (sheet * rows(pmlca) * cols(pmlca))
+                    + (row * cols(pmlca)) + col;
+    pointer[index] = val;
+}
 
 
-/*
- * No advanced error handling. Just assert that everything is ok.
- */
+void put4D(pMLCA pmlca,
+           int64_t block, int64_t sheet,
+           int64_t row, int64_t col, f32 val) {
+    validateMLCA(pmlca);
+    assert(rank(pmlca) == 4);
+    f32 *pointer = ((f32 *) (pmlca->data));
+    int64_t index =
+            (block * sheets(pmlca) * rows(pmlca) * cols(pmlca))
+            + (sheet * rows(pmlca) * cols(pmlca))
+            + (row * cols(pmlca)) + col;
+    pointer[index] = val;
+}
 
 
-/*                _       _  */
-/*  _ _ __ _ _ _ | |_____/ | */
-/* | '_/ _` | ' \| / /___| | */
-/* |_| \__,_|_||_|_\_\   |_| */
-
-
-pA1 A1_alloc(int cols) {
-    assert(cols >= 1);
-    pA1 result = malloc(sizeof(A1));
+pMLCA alloc1D(int64_t cols) {
+    assert(cols <= MLC_MAX_COLS);
+    pMLCA result = malloc(MLC_SIZE);
     assert(result != NULL);
-    result->cols = cols;
-    result->p_storage = malloc(cols * MLC_DATUM_SIZE);
-    assert(result->p_storage != NULL);
+    size_t total_size = cols * MLC_DATUM_SIZE;
+    result->total_size = total_size;
+    result->data = malloc(total_size);
+    assert(result->data != NULL);
+    memset(result->ne, 0, MLC_MAX_RANK * sizeof(result->ne[0]));
+    result->ne[MLC_COLS_IDX] = cols;
+    generate_name(result);
+    validateMLCA(result);
     return result;
 }
 
 
-/*
- * Return a null pointer to help calling code to avoid using
- * deallocated memory.
- *
- * On Mac, run "leaks" on the executable.
- *
- * leaks --atExit -- ./mlc_clib
- */
-pA1 A1_free(pA1 it) {
-    A1_validate(it);
-    memset(it->p_storage, 0, it->cols * MLC_DATUM_SIZE);
-    free(it->p_storage);
-    it->p_storage = NULL;
-    it->cols = 0;
-    free(it);
-    return NULL;
-}
-
-
-void A1_dump(pA1 it) {
-    A1_validate(it);
-    printf("\n1D DUMP\n");
-    printf("cols = %d\n", it->cols);
-    for (int i = 0; i < it->cols; i++) {
-        printf("%.0f ", it->p_storage[i]);
-    }
-    printf("\n");
-}
-
-
-void A1_validate(pA1 it) {
-    assert(it != NULL);
-    assert(it->cols >= 1);
-    assert(it->p_storage != NULL);
-}
-
-
-/*                _      ___  */
-/*  _ _ __ _ _ _ | |____|_  ) */
-/* | '_/ _` | ' \| / /___/ /  */
-/* |_| \__,_|_||_|_\_\  /___| */
-
-
-pA2 A2_alloc(int rows, int cols) {
-    assert(rows >= 1);
-    assert(cols >= 1);
-    pA2 result = malloc(sizeof(A2));
+pMLCA alloc2D(int64_t rows, int64_t cols) {
+    assert(rows <= MLC_MAX_ROWS);
+    assert(cols <= MLC_MAX_COLS);
+    pMLCA result = malloc(MLC_SIZE);
     assert(result != NULL);
-    result->pp_storage = malloc(rows * sizeof(f32 *));
-    assert(result->pp_storage != NULL);
-    for (int i = 0; i < rows; i++) {
-        result->pp_storage[i] = malloc(cols * MLC_DATUM_SIZE);
-        assert(result->pp_storage[i] != NULL);
-    }
-    result->rows = rows;
-    result->cols = cols;
+    size_t total_size = rows * cols * MLC_DATUM_SIZE;
+    result->total_size = total_size;
+    result->data = malloc(total_size);
+    assert(result->data != NULL);
+    memset(result->ne, 0, MLC_MAX_RANK * sizeof(result->ne[0]));
+    result->ne[MLC_COLS_IDX] = cols;
+    result->ne[MLC_ROWS_IDX] = rows;
+    generate_name(result);
+    validateMLCA(result);
     return result;
 }
 
 
-pA2 A2_free(pA2 it) {
-    A2_validate(it);
-    for (int i = 0; i < it->rows; i++) {
-        memset(it->pp_storage[i], 0, it->cols * MLC_DATUM_SIZE);
-        free(it->pp_storage[i]);
-    }
-    memset(it->pp_storage, 0, it->rows * sizeof(f32 *));
-    free(it->pp_storage);
-    it->rows = 0;
-    it->cols = 0;
-    free(it);
-    return NULL;
-}
-
-
-void A2_validate(pA2 it) {
-    assert(it != NULL);
-    assert(it->rows >= 1);
-    assert(it->cols >= 1);
-    assert(it->pp_storage != NULL);
-    for (int i = 0; i < it->rows; i++) {
-        assert(it->pp_storage[i] != NULL);
-    }
-}
-
-
-void A2_dump(pA2 it) {
-    A2_validate(it);
-    printf("\n2D DUMP\n");
-    printf("rows = %d, cols = %d\n", it->rows, it->cols);
-    for (int i = 0; i < it->rows; i++) {
-        printf("row[%d]\t", i);
-        for (int j = 0; j < it->cols; j++) {
-            printf("%.0f ", it->pp_storage[i][j]);
-        }
-        printf("\n");
-    }
-}
-
-
-/*                _      ____ */
-/*  _ _ __ _ _ _ | |____|__ / */
-/* | '_/ _` | ' \| / /___|_ \ */
-/* |_| \__,_|_||_|_\_\  |___/ */
-
-
-pA3 A3_alloc(int rows, int cols, int sheets) {
-    assert(rows >= 1);
-    assert(cols >= 1);
-    assert(sheets >= 1);
-    pA3 result = malloc(sizeof(A3));
+pMLCA alloc3D(int64_t sheets,
+              int64_t rows, int64_t cols) {
+    assert(rows <= MLC_MAX_ROWS);
+    assert(cols <= MLC_MAX_COLS);
+    assert(sheets <= MLC_MAX_SHEETS);
+    pMLCA result = malloc(MLC_SIZE);
     assert(result != NULL);
-    result->ppp_storage = malloc(sheets * sizeof(f32 **));
-    assert(result->ppp_storage != NULL);
-    for (int k = 0; k < sheets; k++) {
-        result->ppp_storage[k] = malloc(rows * sizeof(f32 *));
-        assert(result->ppp_storage[k] != NULL);
-        for (int i = 0; i < rows; i++) {
-            result->ppp_storage[k][i] = malloc(cols * MLC_DATUM_SIZE);
-            assert(result->ppp_storage[k][i] != NULL);
-        }
-    }
-    result->rows = rows;
-    result->cols = cols;
-    result->sheets = sheets;
+    size_t total_size = sheets * rows * cols * MLC_DATUM_SIZE;
+    result->total_size = total_size;
+    result->data = malloc(total_size);
+    assert(result->data != NULL);
+    memset(result->ne, 0, MLC_MAX_RANK * sizeof(result->ne[0]));
+    result->ne[MLC_COLS_IDX] = cols;
+    result->ne[MLC_ROWS_IDX] = rows;
+    result->ne[MLC_SHEETS_IDX] = sheets;
+    generate_name(result);
+    validateMLCA(result);
     return result;
 }
 
 
-pA3 A3_free(pA3 it) {
-    A3_validate(it);
-    for (int k = 0; k < it->sheets; k++) {
-        for (int i = 0; i < it->rows; i++) {
-            memset(it->ppp_storage[k][i], 0, it->cols * MLC_DATUM_SIZE);
-            free(it->ppp_storage[k][i]);
-        }
-        memset(it->ppp_storage[k], 0, it->rows * sizeof(f32 *));
-        free(it->ppp_storage[k]);
-    }
-    free(it->ppp_storage);
-    it->sheets = 0;
-    it->rows = 0;
-    it->cols = 0;
-    free(it);
+pMLCA alloc4D(int64_t blocks, int64_t sheets,
+              int64_t rows, int64_t cols) {
+    assert(rows <= MLC_MAX_ROWS);
+    assert(cols <= MLC_MAX_COLS);
+    assert(sheets <= MLC_MAX_SHEETS);
+    assert(blocks <= MLC_MAX_BLOCKS);
+    pMLCA result = malloc(MLC_SIZE);
+    assert(result != NULL);
+    size_t total_size = blocks * sheets * rows * cols * MLC_DATUM_SIZE;
+    result->total_size = total_size;
+    result->data = malloc(total_size);
+    assert(result->data != NULL);
+    memset(result->ne, 0, MLC_MAX_RANK * sizeof(result->ne[0]));
+    result->ne[MLC_COLS_IDX] = cols;
+    result->ne[MLC_ROWS_IDX] = rows;
+    result->ne[MLC_SHEETS_IDX] = sheets;
+    result->ne[MLC_BLOCKS_IDX] = blocks;
+    generate_name(result);
+    validateMLCA(result);
+    return result;
+}
+
+
+pMLCA freeMLCA(pMLCA pmlca) {
+    validateMLCA(pmlca);
+    memset(pmlca->data, 0, pmlca->total_size);
+    free(pmlca->data);
+    memset(pmlca->name, 0, MLC_MAX_NAME);
+    memset(pmlca->ne, 0, MLC_MAX_RANK * (sizeof(int64_t)));
+    pmlca->total_size = 0;
+    free(pmlca);
     return NULL;
 }
 
 
-void A3_validate(pA3 it) {
-    assert(it != NULL);
-    assert(it->rows >= 1);
-    assert(it->cols >= 1);
-    assert(it->sheets >= 1);
-    assert(it->ppp_storage != NULL);
-    for (int k = 0; k < it->sheets; k++) {
-        assert(it->ppp_storage[k] != NULL);
-        for (int i = 0; i < it->rows; i++) {
-            assert(it->ppp_storage[k][i] != NULL);
-        }
-    }
+void generate_name(pMLCA result) {
+    unsigned char buffer[17] = {0};
+    uuid_generate(buffer);
+    // result->name is 64 chars; unparse write 37
+    uuid_unparse(buffer, result->name);
 }
 
 
-void A3_dump(pA3 it) {
-    A3_validate(it);
-    printf("\n3D DUMP\n");
-    printf("rows = %d, cols = %d, sheets = %d\n",
-           it->rows, it->cols, it->sheets);
-    for (int k = 0; k < it->sheets; k++) {
-        printf("\n");
-        for (int i = 0; i < it->rows; i++) {
-            printf("sheet[%d], row[%d]\t", k, i);
-            for (int j = 0; j < it->cols; j++) {
-                printf("%.0f ", it->ppp_storage[k][i][j]);
+void dumpMLCA(pMLCA pmlca) {
+    validateMLCA(pmlca);
+    switch (rank(pmlca)) {
+        case 1:
+            printf("\n1D CONTIG DUMP\n");
+            printf("Array name = %s\n", pmlca->name);
+            printf("cols = %lld, total_size = %ld\n", cols(pmlca), pmlca->total_size);
+            for (int col = 0; col < cols(pmlca); col++) {
+                printf("%.0f ", get1D(pmlca, col));
             }
             printf("\n");
-        }
-    }
-}
-
-
-/*                _       _ _   */
-/*  _ _ __ _ _ _ | |_____| | |  */
-/* | '_/ _` | ' \| / /___|_  _| */
-/* |_| \__,_|_||_|_\_\     |_|  */
-
-
-pA4 A4_alloc(int rows, int cols, int sheets, int blocks) {
-    assert(rows >= 1);
-    assert(cols >= 1);
-    assert(sheets >= 1);
-    assert(blocks >= 1);
-    pA4 result = malloc(sizeof(A4));
-    assert(result != NULL);
-    result->pppp_storage = malloc(sheets * sizeof(f32 ***));
-    assert(result->pppp_storage != NULL);
-    for (int l = 0; l < blocks; l++) {
-        result->pppp_storage[l] = malloc(sheets * sizeof(f32 **));
-        for (int k = 0; k < sheets; k++) {
-            result->pppp_storage[l][k] = malloc(rows * sizeof(f32 *));
-            assert(result->pppp_storage[l][k] != NULL);
-            for (int i = 0; i < rows; i++) {
-                result->pppp_storage[l][k][i] = malloc(cols * MLC_DATUM_SIZE);
-                assert(result->pppp_storage[l][k][i] != NULL);
-            }
-        }
-    }
-    result->rows = rows;
-    result->cols = cols;
-    result->sheets = sheets;
-    result->blocks = blocks;
-    return result;
-}
-
-
-pA4 A4_free(pA4 it) {
-    A4_validate(it);
-    for (int l = 0; l < it->blocks; l++) {
-        for (int k = 0; k < it->sheets; k++) {
-            for (int i = 0; i < it->rows; i++) {
-                memset(it->pppp_storage[l][k][i], 0, it->cols * MLC_DATUM_SIZE);
-                free(it->pppp_storage[l][k][i]);
-            }
-            memset(it->pppp_storage[l][k], 0, it->rows * sizeof(f32 *));
-            free(it->pppp_storage[l][k]);
-        }
-        memset(it->pppp_storage[l], 0, it->sheets * sizeof(f32 **));
-        free(it->pppp_storage[l]);
-    }
-    free(it->pppp_storage);
-    it->blocks = 0;
-    it->sheets = 0;
-    it->rows = 0;
-    it->cols = 0;
-    free(it);
-    return NULL;
-}
-
-
-void A4_validate(pA4 it) {
-    assert(it != NULL);
-    assert(it->rows >= 1);
-    assert(it->cols >= 1);
-    assert(it->sheets >= 1);
-    assert(it->blocks >= 1);
-    assert(it->pppp_storage != NULL);
-    for (int l = 0; l < it->blocks; l++) {
-        assert(it->pppp_storage[l] != NULL);
-        for (int k = 0; k < it->sheets; k++) {
-            assert(it->pppp_storage[l][k] != NULL);
-            for (int i = 0; i < it->rows; i++) {
-                assert(it->pppp_storage[l][k][i] != NULL);
-            }
-        }
-    }
-}
-
-
-void A4_dump(pA4 it) {
-    A4_validate(it);
-    printf("\n4D DUMP\n");
-    printf("blocks = %d, sheets = %d, rows = %d, cols = %d\n",
-           it->blocks, it->sheets, it->rows, it->cols);
-    for (int l = 0; l < it->blocks; l++) {
-        printf("\n");
-        for (int k = 0; k < it->sheets; k++) {
-            printf("\n");
-            for (int i = 0; i < it->rows; i++) {
-                printf("block[%d], sheet[%d], row[%d]\t",
-                       l, k, i);
-                for (int j = 0; j < it->cols; j++) {
-                    printf("%.0f ", it->pppp_storage[l][k][i][j]);
+            break;
+        case 2:
+            printf("\n2D CONTIG DUMP\n");
+            printf("Array name = %s\n", pmlca->name);
+            printf("cols = %lld, rows = %lld, total_size = %ld\n",
+                   cols(pmlca), rows(pmlca), pmlca->total_size);
+            for (int row = 0; row < rows(pmlca); row++) {
+                printf("row[%d]\t", row);
+                for (int col = 0; col < cols(pmlca); col++) {
+                    printf("%.0f ", get2D(pmlca, row, col));
                 }
                 printf("\n");
             }
-        }
+            break;
+        case 3:
+            printf("\n3D CONTIG DUMP\n");
+            printf("Array name = %s\n", pmlca->name);
+            printf("cols = %lld, rows = %lld, sheets = %lld, total_size = %ld\n",
+                   cols(pmlca), rows(pmlca), sheets(pmlca), pmlca->total_size);
+            for (int sheet = 0; sheet < sheets(pmlca); sheet++) {
+                printf("\n");
+                for (int row = 0; row < rows(pmlca); row++) {
+                    printf("sheet[%d], row[%d]\t", sheet, row);
+                    for (int col = 0; col < cols(pmlca); col++) {
+                        printf("%.0f ", get3D(
+                                pmlca, sheet,
+                                row, col));
+                    }
+                    printf("\n");
+                }
+            }
+            break;
+        case 4:
+            printf("\n4D CONTIG DUMP\n");
+            printf("Array name = %s\n", pmlca->name);
+            printf("cols = %lld, rows = %lld, sheets = %lld, blocks = %lld, total_size = %ld\n",
+                   cols(pmlca), rows(pmlca), sheets(pmlca), blocks(pmlca),
+                   pmlca->total_size);
+            for (int block = 0; block < blocks(pmlca); block++) {
+                printf("\n");
+                for (int sheet = 0; sheet < sheets(pmlca); sheet++) {
+                    printf("\n");
+                    for (int row = 0; row < rows(pmlca); row++) {
+                        printf("block[%d], sheet[%d], row[%d]\t", block, sheet, row);
+                        for (int col = 0; col < cols(pmlca); col++) {
+                            printf("%.0f ", get4D(
+                                    pmlca, block, sheet,
+                                    row, col));
+                        }
+                        printf("\n");
+                    }
+                }
+            }
+            break;
+        default:
+            assert(0);
     }
 }
 
@@ -463,8 +400,3 @@ void A4_dump(pA4 it) {
  *      7  . . . . . . . . . . . . . . . . . . . . . . . . . . . .
  */
 
-
-pA3 Conv2D(pA4 boxcars, pA2 Input) {
-    pA3 result = NULL;
-    return result;
-}
