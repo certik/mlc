047b5f294da790cadb45239ae090e700a8d896fd
Ondřej Čertík <ondrej@certik.us>
2024-03-14 15:13:41 -0600

    Fix a shape

--- mlc_clib/inference-generated.c
+++ mlc_clib/inference-generated.c
@@ -9,7 +9,7 @@ void inference_calculation(
         f32 *out,     // (10,)
         f32 *kernel1, // (32, 1, 3, 3)
         f32 *bias1,   // (32,)
-        f32 *kernel2, // (3, 3, 32, 64)
+        f32 *kernel2, // (32, 64, 3, 3)
         f32 *bias2,   // (64,)
         f32 *dense_w, // (1600, 10)
         f32 *dense_b, // (10,)
