4d4ebd162fab256b372cc9431897a65fb3139a8f
Ondřej Čertík <ondrej@certik.us>
2024-03-14 14:27:13 -0600

    Separate temporaries allocation

--- mlc_clib/inference-generated.c
+++ mlc_clib/inference-generated.c
@@ -4,8 +4,7 @@
 #include "inference-generated.h"
 #include "kernels.h"
 
-
-void inference(
+void inference_calculation(
         f32 *in,      // (1, 28, 28)
         f32 *out,     // (10,)
         f32 *kernel1, // (32, 1, 3, 3)
@@ -13,17 +12,15 @@ void inference(
         f32 *kernel2, // (3, 3, 32, 64)
         f32 *bias2,   // (64,)
         f32 *dense_w, // (1600, 10)
-        f32 *dense_b // (10,)
+        f32 *dense_b, // (10,)
+        f32 *out2,    // (32, 26, 26)
+        f32 *out3,    // (32, 26, 26)
+        f32 *out4,    // (32, 13, 13)
+        f32 *out5,    // (64, 11, 11)
+        f32 *out6,    // (64, 11, 11)
+        f32 *out7,    // (64, 5, 5)
+        f32 *out8     // (10,)
     ) {
-    // Temporary arrays
-    f32 *out2 = malloc(32*26*26*sizeof(f32));
-    f32 *out3 = malloc(32*26*26*sizeof(f32));
-    f32 *out4 = malloc(32*13*13*sizeof(f32));
-    f32 *out5 = malloc(64*11*11*sizeof(f32));
-    f32 *out6 = malloc(64*11*11*sizeof(f32));
-    f32 *out7 = malloc(64*5*5*sizeof(f32));
-    f32 *out8 = malloc(10*sizeof(f32));
-
     // Conv2D
     conv2d(1, 32, 3,
         28, 28,
@@ -80,5 +77,41 @@ void inference(
     softmax(10,
             out8, // (10,)
             out   // (10,)
+        );
+}
+
+void allocate_temporaries(
+        f32 **out2,
+        f32 **out3,
+        f32 **out4,
+        f32 **out5,
+        f32 **out6,
+        f32 **out7,
+        f32 **out8
+) {
+    *out2 = malloc(32*26*26*sizeof(f32));
+    *out3 = malloc(32*26*26*sizeof(f32));
+    *out4 = malloc(32*13*13*sizeof(f32));
+    *out5 = malloc(64*11*11*sizeof(f32));
+    *out6 = malloc(64*11*11*sizeof(f32));
+    *out7 = malloc(64*5*5*sizeof(f32));
+    *out8 = malloc(10*sizeof(f32));
+}
+
+void inference(
+        f32 *in,      // (1, 28, 28)
+        f32 *out,     // (10,)
+        f32 *kernel1, // (32, 1, 3, 3)
+        f32 *bias1,   // (32,)
+        f32 *kernel2, // (3, 3, 32, 64)
+        f32 *bias2,   // (64,)
+        f32 *dense_w, // (1600, 10)
+        f32 *dense_b  // (10,)
+) {
+    f32 *out2, *out3, *out4, *out5, *out6, *out7, *out8;
+    allocate_temporaries(&out2, &out3, &out4, &out5, &out6, &out7, &out8);
+    inference_calculation(in, out, kernel1, bias1, kernel2, bias2,
+        dense_w, dense_b,
+        out2, out3, out4, out5, out6, out7, out8
         );
 }
