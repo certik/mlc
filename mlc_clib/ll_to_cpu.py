fe287fc7d0cc24fd7d08f07f968b5ed33822c1d5
Ondřej Čertík <ondrej@certik.us>
2024-03-14 19:45:47 -0600

    Generate inference args

--- mlc_clib/ll_to_cpu.py
+++ mlc_clib/ll_to_cpu.py
@@ -63,6 +63,11 @@ class LLToCPUVisitor:
         self.inf_calc_args = "        " + ",\n        ".join(args)
 
         args = []
+        for var in x.x_in + x.x_out + x.weights:
+            args.append(f"f32 *{var.name} /*{var.shape}*/")
+        self.inf_args = "        " + ",\n        ".join(args)
+
+        args = []
         for var in x.temporaries:
             args.append(f"f32 **{var.name} /*{var.shape}*/")
         self.inf_alloc_temp_args = "        " + ",\n        ".join(args)
@@ -92,14 +97,7 @@ void allocate_temporaries(
 }}
 
 void inference(
-        f32 *in,      // (1, 28, 28)
-        f32 *out,     // (10,)
-        f32 *kernel1, // (32, 1, 3, 3)
-        f32 *bias1,   // (32,)
-        f32 *kernel2, // (3, 3, 32, 64)
-        f32 *bias2,   // (64,)
-        f32 *dense_w, // (1600, 10)
-        f32 *dense_b  // (10,)
+{self.inf_args}
 ) {{
     f32 *out2, *out3, *out4, *out5, *out6, *out7, *out8;
     allocate_temporaries(&out2, &out3, &out4, &out5, &out6, &out7, &out8);
