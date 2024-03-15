8d89ac942bcdc2f8154fa3571a04be7e460afe25
Ondřej Čertík <ondrej@certik.us>
2024-03-15 07:28:35 -0600

    Run just one epoch

--- examples/mnist/mnist-tf.py
+++ examples/mnist/mnist-tf.py
@@ -43,7 +43,7 @@ def train(model_name):
 
     model.summary()
     batch_size = 128
-    epochs = 2
+    epochs = 1
     model.compile(loss="categorical_crossentropy", optimizer="adam", metrics=["accuracy"])
     model.fit(x_train, y_train, batch_size=batch_size, epochs=epochs, validation_split=0.1)
 
