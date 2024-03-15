66e54e133f35d330de331644aab49fbc462109ef
Ondřej Čertík <ondrej@certik.us>
2024-03-14 19:46:24 -0600

    Copy over generated file

--- mlc_clib/inference-generated.c
+++ mlc_clib/inference-generated.c
@@ -81,14 +81,14 @@ void allocate_temporaries(
 }
 
 void inference(
-        f32 *in,      // (1, 28, 28)
-        f32 *out,     // (10,)
-        f32 *kernel1, // (32, 1, 3, 3)
-        f32 *bias1,   // (32,)
-        f32 *kernel2, // (3, 3, 32, 64)
-        f32 *bias2,   // (64,)
-        f32 *dense_w, // (1600, 10)
-        f32 *dense_b  // (10,)
+        f32 *in /*(1, 28, 28)*/,
+        f32 *out /*(10,)*/,
+        f32 *kernel1 /*(32, 1, 3, 3)*/,
+        f32 *bias1 /*(32,)*/,
+        f32 *kernel2 /*(64, 32, 3, 3)*/,
+        f32 *bias2 /*(64,)*/,
+        f32 *dense_w /*(10, 1600)*/,
+        f32 *dense_b /*(10,)*/
 ) {
     f32 *out2, *out3, *out4, *out5, *out6, *out7, *out8;
     allocate_temporaries(&out2, &out3, &out4, &out5, &out6, &out7, &out8);
