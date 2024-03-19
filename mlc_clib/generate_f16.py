62265403378714753f84ee0c3d94abde858c3fad
Ondřej Čertík <ondrej@certik.us>
2024-03-18 19:43:53 -0600

    Use relu_f16

--- mlc_clib/generate_f16.py
+++ mlc_clib/generate_f16.py
@@ -1,7 +1,7 @@
 from llir import (Inference, Array, f16, f32, conv2d, relu, max_pool_2d,
         reshape, saxpy, saxpy_f16, softmax, pad_32K_copy, cast_32K_f16_f32,
         cast_32K_f32_f16, section_32K_copy, relu_32K_f16, cast_f32_f16,
-        softmax_f16, cast_f16_f32, max_pool_2d_f16)
+        softmax_f16, cast_f16_f32, max_pool_2d_f16, relu_f16)
 from ll_to_cpu import ll_to_cpu
 
 ll = Inference(
@@ -23,25 +23,12 @@ ll = Inference(
             Array("tmp2", f32(), (32, 26, 26)),
             Array("tmp3", f32(), (32, 26, 26)),
             Array("tmp4", f32(), (32, 13, 13)),
-
             Array("tmp5", f32(), (64, 11, 11)),
-            Array("tmp5b", f32(), (32768,)),
-            Array("tmp5c", f16(), (32768,)),
-            Array("tmp5d", f16(), (32768,)),
-            Array("tmp5e", f32(), (32768,)),
-
-            Array("tmp6", f32(), (64, 11, 11)),
-            Array("tmp6c", f16(), (64, 11, 11)),
-
+            Array("tmp5b", f16(), (64*11*11,)),
+            Array("tmp6", f16(), (64, 11, 11)),
             Array("tmp7", f16(), (64, 5, 5)),
-            Array("tmp7b", f32(), (32768,)),
-            Array("tmp7c", f16(), (1600,)),
-            Array("tmp7d", f16(), (10,)),
-            Array("tmp7e", f32(), (10,)),
-
-            Array("tmp8", f32(), (10,)),
-            Array("tmp8b", f16(), (10,)),
-            Array("tmp8c", f16(), (10,)),
+            Array("tmp7b", f16(), (10,)),
+            Array("tmp8", f16(), (10,)),
         ],
         # Verify pass: the array arguments fully determine the parameters
         instructions=[
@@ -50,19 +37,13 @@ ll = Inference(
             max_pool_2d(32, 26, 26, "tmp3", "tmp4"),
             conv2d(32, 64, 3, 13, 13, "kernel2", "bias2", "tmp4", "tmp5"),
 
-            #relu(64, 11, 11, "tmp5", "tmp6"),
-            pad_32K_copy(7744, "tmp5", "tmp5b"),
-            cast_32K_f32_f16("tmp5b", "tmp5c"),
-            relu_32K_f16("tmp5c", "tmp5d"),
-            cast_32K_f16_f32("tmp5d", "tmp5e"),
-            section_32K_copy(7744, "tmp5e", "tmp6"),
+            cast_f32_f16(64*11*11, "tmp5", "tmp5b"),
+            relu_f16(64*11*11, "tmp5b", "tmp6"),
+            max_pool_2d_f16(64, 11, 11, "tmp6", "tmp7"),
+            saxpy_f16(10, 1600, "dense_w", "dense_b", "tmp7", "tmp7b"),
+            softmax_f16(10, "tmp7b", "tmp8"),
 
-            cast_f32_f16(7744, "tmp6", "tmp6c"),
-            max_pool_2d_f16(64, 11, 11, "tmp6c", "tmp7"),
-            saxpy_f16(10, 1600, "dense_w", "dense_b", "tmp7", "tmp7d"),
-            softmax_f16(10, "tmp7d", "tmp8c"),
-
-            cast_f16_f32(10, "tmp8c", "out"),
+            cast_f16_f32(10, "tmp8", "out"),
         ]
     )
 
--- mlc_clib/ll_to_cpu.py
+++ mlc_clib/ll_to_cpu.py
@@ -39,7 +39,8 @@ class LLToCPUVisitor:
 
     def visit(self, x):
         supported_nodes = ["Inference", "conv2d",
-                           "relu", "max_pool_2d", "max_pool_2d_f16",
+                           "relu", "relu_f16",
+                           "max_pool_2d", "max_pool_2d_f16",
                            "reshape", "saxpy", "saxpy_f16",
                            "softmax", "softmax_f16",
                            "pad_32K_copy", "section_32K_copy",
@@ -158,6 +159,14 @@ void inference_calculation(
     def visit_relu(self, x):
         self.inf_body += f"""\
     relu({x.in_channels}, {x.H}, {x.W},
+        {x.x_in}, // {self.tmpinout[x.x_in].shape}
+        {x.x_out} // {self.tmpinout[x.x_out].shape}
+    );
+"""
+
+    def visit_relu_f16(self, x):
+        self.inf_body += f"""\
+    relu_f16({x.n},
         {x.x_in}, // {self.tmpinout[x.x_in].shape}
         {x.x_out} // {self.tmpinout[x.x_out].shape}
     );
