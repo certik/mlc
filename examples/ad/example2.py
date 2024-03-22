120b6b11fbcf7989a8195bfebc995a1c5e16b0f7
Ondřej Čertík <ondrej@certik.us>
2024-03-22 09:23:17 -0600

    Add comments

--- examples/ad/example2.py
+++ examples/ad/example2.py
@@ -12,6 +12,7 @@ y = torch.zeros(3)  # expected output
 w = torch.tensor(w0, requires_grad=True)
 b = torch.tensor(b0, requires_grad=True)
 z = torch.matmul(x, w)+b
+#z.retain_grad()
 loss1 = torch.nn.functional.binary_cross_entropy_with_logits(z, y)
 loss2 = torch.nn.functional.l1_loss(z, y)
 loss3 = torch.sum(torch.abs(z-y))
@@ -24,6 +25,31 @@ print("Derivatives")
 loss3.backward()
 print(w.grad)
 print(b.grad)
+
+# d|x|/dx = sign(x)
+
+# L3 = sum |z-y|
+# z = X W + b
+# z = (z1, z2, ..., zn) = (z^i)
+# W = (w^ij)
+# X = (x^i)
+# ∂(XW)^k/∂w^ij = ∂x_lw^lk/∂w^ij = x_l ∂w^lk/∂w^ij = x_l delta_li delta_kj
+#     = x_i delta_kj
+
+# ∂L3/∂z1 = ∂|z1-y1|/∂z1 = sign(z1)
+# Index notation:
+# ∂L3/∂zi = ∂sum|z-y|/∂zi = sign(zi)
+# Vector notation:
+# ∂L3/∂z = ∂sum|z-y|/∂z = sign(z)
+
+# Index notation:
+# ∂L3/∂bi = ∂L3/∂z^j * ∂z^j/∂b^i = sign(z^j) * delta_{ji} = sign(zi)
+# Vector notation:
+# ∂L3/∂b = ∂L3/∂z * ∂z/∂b = sign(z) * 1
+
+# Index notation:
+# ∂L3/∂wij = ∂L3/∂z^k * ∂z^k/∂w^ij = sign(z^k) * x_i delta_{kj}
+#     = x_i * sign(z^j)
 
 print()
 
