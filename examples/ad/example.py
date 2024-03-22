f755c5adb675adbf002f3ec14aef588758770333
Ondřej Čertík <ondrej@certik.us>
2024-03-21 18:43:34 -0600

    Rename sdiff to fsdiff

--- examples/ad/example.py
+++ examples/ad/example.py
@@ -8,7 +8,7 @@ class Integer:
     def n(self, d):
         return self.i
 
-    def sdiff(self, x):
+    def fsdiff(self, x):
         return Integer(0)
 
     def ndiff(self, x, d):
@@ -34,7 +34,7 @@ class Symbol:
     def n(self, d):
         return d[self]
 
-    def sdiff(self, x):
+    def fsdiff(self, x):
         if x == self:
             return Integer(1)
         else:
@@ -52,8 +52,16 @@ class Symbol:
     def bndiff(self, deriv, d):
         self.partial += deriv
 
-    def bsdiff(self, deriv):
-        self.partial = Add(self.partial, deriv)
+    # u = x
+    # Input: dLdu = ∂L/∂u
+    # bsdiff computes:
+    # ∂L/∂x = ∂L/∂u = dLdu
+    # We accumulate ∂L/∂x.
+    def bsdiff(self, dLdu):
+        if self.partial == 0:
+            self.partial = dLdu
+        else:
+            self.partial = Add(self.partial, dLdu)
 
     def __str__(self):
         return f"{self.name}"
@@ -67,8 +75,8 @@ class Add:
     def n(self, d):
         return self.left.n(d) + self.right.n(d)
 
-    def sdiff(self, x):
-        return Add(self.left.sdiff(x), self.right.sdiff(x))
+    def fsdiff(self, x):
+        return Add(self.left.fsdiff(x), self.right.fsdiff(x))
 
     def ndiff(self, x, d):
         return self.left.ndiff(x, d) + self.right.ndiff(x, d)
@@ -81,9 +89,15 @@ class Add:
         self.left.bndiff(deriv, d)
         self.right.bndiff(deriv, d)
 
-    def bsdiff(self, deriv):
-        self.left.bsdiff(deriv)
-        self.right.bsdiff(deriv)
+    # u = v + w
+    # Input: dLdu = ∂L/∂u
+    # bsdiff computes:
+    # ∂L/∂v = ∂L/∂u * ∂u/∂v = dLdu * 1 = dLdu
+    # ∂L/∂w = ∂L/∂u * ∂u/∂w = dLdu * 1 = dLdu
+    # We pass ∂L/∂v and ∂L/∂w down the chain.
+    def bsdiff(self, dLdu):
+        self.left.bsdiff(dLdu)
+        self.right.bsdiff(dLdu)
 
     def __str__(self):
         return f"({self.left} + {self.right})"
@@ -97,9 +111,9 @@ class Mul:
     def n(self, d):
         return self.left.n(d) * self.right.n(d)
 
-    def sdiff(self, x):
-        return Add(Mul(self.left.sdiff(x),self.right),
-                Mul(self.left, self.right.sdiff(x)))
+    def fsdiff(self, x):
+        return Add(Mul(self.left.fsdiff(x),self.right),
+                Mul(self.left, self.right.fsdiff(x)))
 
     def ndiff(self, x, d):
         return self.left.ndiff(x,d)*self.right.n(d) + \
@@ -113,9 +127,15 @@ class Mul:
         self.left.bndiff(self.right.n(d)*deriv, d)
         self.right.bndiff(self.left.n(d)*deriv, d)
 
-    def bsdiff(self, deriv):
-        self.left.bsdiff(Mul(self.right,deriv))
-        self.right.bsdiff(Mul(self.left,deriv))
+    # u = v*w
+    # Input: dLdu = ∂L/∂u
+    # bsdiff computes:
+    # ∂L/∂v = ∂L/∂u * ∂u/∂v = dLdu * w
+    # ∂L/∂w = ∂L/∂u * ∂u/∂w = dLdu * v
+    # We pass ∂L/∂v and ∂L/∂w down the chain.
+    def bsdiff(self, dLdu):
+        self.left.bsdiff(Mul(dLdu, self.right))
+        self.right.bsdiff(Mul(dLdu, self.left))
 
     def __str__(self):
         return f"({self.left} * {self.right})"
@@ -128,8 +148,8 @@ class Sin:
     def n(self, d):
         return math.sin(self.x.n(d))
 
-    def sdiff(self, x):
-        return Mul(Cos(self.x), self.x.sdiff(x))
+    def fsdiff(self, x):
+        return Mul(Cos(self.x), self.x.fsdiff(x))
 
     def ndiff(self, x, d):
         return math.cos(self.x.n(d)) * self.x.ndiff(x,d)
@@ -144,22 +164,22 @@ class Sin:
     # u = sin(v)
     # where `v` can be a function of other variables (e.g., v = f(x, y, z)).
 
-    # sdiff: Forward mode:
+    # fsdiff: Forward mode:
     # Input: x = x
-    # Output: ∂u/∂x = ∂u/∂v * ∂v/∂x = cos(v) * v.sdiff(x)
+    # Output: ∂u/∂x = ∂u/∂v * ∂v/∂x = cos(v) * v.fsdiff(x)
     # The function returns the output directly, and uses `v` in the process
 
     # bsdiff: Reverse mode:
     # Input: dLdu = ∂L/∂u
-    # bsdiff computes: ∂L/∂v = ∂L/∂u * ∂u/∂v = deriv * cos(v)
+    # bsdiff computes: ∂L/∂v = ∂L/∂u * ∂u/∂v = dLdu * cos(v)
     # The function does not return directly, rather it passes ∂L/∂v down to `v`
     # via.bsdiff(∂L/∂v). Eventually it reaches `x` (via multiple paths in
     # general) and we keep summing (accumulating) the expressions we get there.
     # The function does not call any other `bsdiff` recursively in order to
     # compute ∂L/∂v. It only calls `bsdiff` to pass the result down the tree.
 
-    def sdiff(self, x):
-        return Mul(Cos(self.x), self.x.sdiff(x))
+    def fsdiff(self, x):
+        return Mul(Cos(self.x), self.x.fsdiff(x))
 
     def bsdiff(u, dLdu):
         v = u.x
@@ -177,8 +197,8 @@ class Cos:
     def n(self, d):
         return math.cos(self.x.n(d))
 
-    def sdiff(self, x):
-        return Mul(Mul(Integer(-1), Sin(self.x)), self.x.sdiff(x))
+    def fsdiff(self, x):
+        return Mul(Mul(Integer(-1), Sin(self.x)), self.x.fsdiff(x))
 
     def ndiff(self, x, d):
         return -math.sin(self.x.n(d)) * self.x.ndiff(x, d)
@@ -194,8 +214,13 @@ class Square:
     def n(self, d):
         return self.x.n(d)**2
 
-    def sdiff(self, x):
-        return Mul(Mul(Integer(2), self.x), self.x.sdiff(x))
+    # u = v^2
+    # Input: x
+    # fsdiff computes:
+    # ∂u/∂x = ∂u/∂v * ∂v/∂x = 2*v * v.fsdiff(x)
+    # We return ∂u/∂x.
+    def fsdiff(self, x):
+        return Mul(Mul(Integer(2), self.x), self.x.fsdiff(x))
 
     def ndiff(self, x, d):
         return 2 * self.x.n(d) * self.x.ndiff(x,d)
@@ -206,8 +231,13 @@ class Square:
     def bndiff(self, deriv, d):
         self.x.bndiff(2*self.x.n(d)*deriv, d)
 
-    def bsdiff(self, deriv):
-        self.x.bsdiff(Mul(Mul(Integer(2),self.x),deriv))
+    # u = v^2
+    # Input: dLdu = ∂L/∂u
+    # bsdiff computes:
+    # ∂L/∂v = ∂L/∂u * ∂u/∂v = dLdu * 2*v
+    # We pass ∂L/∂v down the chain.
+    def bsdiff(self, dLdu):
+        self.x.bsdiff(Mul(dLdu, Mul(Integer(2),self.x)))
 
     def __str__(self):
         return f"({self.x})**2"
@@ -220,8 +250,8 @@ print(L)
 vals = {x: 0.1, y: 0.3}
 print("L =", L.n(vals))
 print("Forward:")
-print("∂L/∂x =", L.sdiff(x)) # y + 2*sin(x)*cos(x)
-print("∂L/∂y =", L.sdiff(y)) # x
+print("∂L/∂x =", L.fsdiff(x)) # y + 2*sin(x)*cos(x)
+print("∂L/∂y =", L.fsdiff(y)) # x
 print("∂L/∂x =", L.ndiff(x, vals)) # 0.49866933079506126
 print("∂L/∂y =", L.ndiff(y, vals)) # 0.1
 print("Backward:")
@@ -243,8 +273,8 @@ vals = {x: 2, y: 3}
 
 print("z =", z.n(vals))             # Output: z = 19
 print("Forward:")
-print("∂z/∂x =", z.sdiff(x))  # Output: 2*x + y
-print("∂z/∂y =", z.sdiff(y))  # Output: x + 2*y
+print("∂z/∂x =", z.fsdiff(x))  # Output: 2*x + y
+print("∂z/∂y =", z.fsdiff(y))  # Output: x + 2*y
 print("∂z/∂x =", z.ndiff(x, vals))  # Output: ∂z/∂x = 7
 print("∂z/∂y =", z.ndiff(y, vals))  # Output: ∂z/∂y = 8
 print("Backward:")
@@ -265,8 +295,8 @@ print()
 L = Mul(Mul(x, y), Square(Square(Sin(x))))
 print("L =", L)
 print("Forward:")
-print("∂L/∂x =", L.sdiff(x))
-print("∂L/∂y =", L.sdiff(y))
+print("∂L/∂x =", L.fsdiff(x))
+print("∂L/∂y =", L.fsdiff(y))
 print("Backward:")
 L.bclean()
 L.bsdiff(Integer(1))
