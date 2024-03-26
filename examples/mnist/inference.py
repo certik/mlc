6280eb4d893b7f79d323d74900948d8cb5e2a858
Ondřej Čertík <ondrej@certik.us>
2024-03-26 16:49:10 -0600

    Add TF inference

--- examples/mnist/inference.py
+++ examples/mnist/inference.py
@@ -50,75 +50,34 @@ def gguf_to_array(g, expected_name):
     # The GGUF format stores the shape in reversed order
     return np.reshape(g.data, np.flip(g.shape))
 
-def run_model_pt(inp,
-        kernel1, bias1, kernel2, bias2, kernel3, bias3, kernel4, bias4,
-        batchnorm1_moving_mean,
-            batchnorm1_moving_variance,
-        batchnorm2_gamma, batchnorm2_beta, batchnorm2_moving_mean,
-            batchnorm2_moving_variance,
-        dense_w, dense_b):
+def run_model_pt(inp, kernel1, bias1, kernel2, bias2, dense_w, dense_b):
     class Model(torch.nn.Module):
         def __init__(self):
             super().__init__()
             self.model = torch.nn.Sequential (
-                torch.nn.Conv2d(1, 32, 5, bias=True),   # 0
+                torch.nn.Conv2d(1, 32, 3, bias=True),
                 torch.nn.ReLU(),
-                torch.nn.Conv2d(32, 32, 5, bias=True),  # 2
+                torch.nn.MaxPool2d((2, 2)),
+                torch.nn.Conv2d(32, 64, 3, bias=True),
                 torch.nn.ReLU(),
-                torch.nn.BatchNorm2d(32, eps=0.001, momentum=0.01,
-                    track_running_stats=False, affine=False), # 4
-                #torch.nn.MaxPool2d((2, 2)),
-
-                #torch.nn.Conv2d(32, 64, 3, bias=True),  # 6
-                #torch.nn.ReLU(),
-                #torch.nn.Conv2d(64, 64, 3, bias=True),  # 8
-                #torch.nn.ReLU(),
-                #torch.nn.BatchNorm2d(64),               # 10
-                #torch.nn.MaxPool2d((2, 2)),
-
-                #torch.nn.Flatten(0, -1),
-                #torch.nn.Linear(576, 10, bias=True),    # 13
-                #torch.nn.Softmax(dim=0),
+                torch.nn.MaxPool2d((2, 2)),
+                torch.nn.Flatten(0, -1),
+                torch.nn.Linear(1600, 10, bias=True),
+                torch.nn.Softmax(dim=0),
                 )
 
             self.model[0].weight = torch.nn.Parameter(torch.from_numpy(
                     kernel1.copy()))
             self.model[0].bias = torch.nn.Parameter(torch.from_numpy(
                     bias1.copy()))
-            self.model[2].weight = torch.nn.Parameter(torch.from_numpy(
+            self.model[3].weight = torch.nn.Parameter(torch.from_numpy(
                     kernel2.copy()))
-            self.model[2].bias = torch.nn.Parameter(torch.from_numpy(
+            self.model[3].bias = torch.nn.Parameter(torch.from_numpy(
                     bias2.copy()))
-            #self.model[4].weight = torch.nn.Parameter(torch.from_numpy(
-            #        batchnorm1_gamma.copy()))
-            #self.model[4].bias = torch.nn.Parameter(torch.from_numpy(
-            #        batchnorm1_beta.copy()))
-            self.model[4].running_mean = torch.nn.Parameter(torch.from_numpy(
-                    batchnorm1_moving_mean.copy()), requires_grad=False)
-            self.model[4].running_var = torch.nn.Parameter(torch.from_numpy(
-                    batchnorm1_moving_variance.copy()), requires_grad=False)
-#
-#            self.model[6].weight = torch.nn.Parameter(torch.from_numpy(
-#                    kernel3.copy()))
-#            self.model[6].bias = torch.nn.Parameter(torch.from_numpy(
-#                    bias3.copy()))
-#            self.model[8].weight = torch.nn.Parameter(torch.from_numpy(
-#                    kernel4.copy()))
-#            self.model[8].bias = torch.nn.Parameter(torch.from_numpy(
-#                    bias4.copy()))
-#            self.model[10].weight = torch.nn.Parameter(torch.from_numpy(
-#                    batchnorm2_gamma.copy()))
-#            self.model[10].bias = torch.nn.Parameter(torch.from_numpy(
-#                    batchnorm2_beta.copy()))
-#            self.model[10].running_mean = torch.nn.Parameter(torch.from_numpy(
-#                    batchnorm2_moving_mean.copy()), requires_grad=False)
-#            self.model[10].running_var = torch.nn.Parameter(torch.from_numpy(
-#                    batchnorm2_moving_variance.copy()), requires_grad=False)
-#
-#            self.model[13].weight = torch.nn.Parameter(torch.from_numpy(
-#                    dense_w.copy()))
-#            self.model[13].bias = torch.nn.Parameter(torch.from_numpy(
-#                    dense_b.copy()))
+            self.model[7].weight = torch.nn.Parameter(torch.from_numpy(
+                    dense_w.copy()))
+            self.model[7].bias = torch.nn.Parameter(torch.from_numpy(
+                    dense_b.copy()))
 
         def forward(self, x):
             return self.model(x)
@@ -127,26 +86,23 @@ def run_model_pt(inp,
     assert inp.shape == (28, 28)
     model = Model()
     inp_ = np.expand_dims(inp, 0)
-    inp_ = np.expand_dims(inp_, 0)
     torch_inp = torch.tensor(inp_)
     torch_out = model(torch_inp)
     out = torch_out.detach().numpy()
     print("Output shape:", out.shape)
-    #assert out.shape == (10,)
-    #print("PT:", out)
-    #print("PT max:", out.argmax())
+    assert out.shape == (10,)
+    print("PT:", out)
+    print("PT max:", out.argmax())
 
     return out
 
-def run_model_tf(inp):
-    tf_model = keras.models.load_model("mnist-cnn-beautiful-model")
-    #tf_model = keras.models.Sequential(tf_model.layers[:8])
+def run_model_tf(inp, kernel1, bias1, kernel2, bias2, dense_w, dense_b):
+    tf_model = keras.models.load_model("mnist-cnn-model")
     print("Input shape:", inp.shape)
     assert inp.shape == (28, 28)
     out_tf = tf_model(np.expand_dims(np.expand_dims(inp, 0), -1))
-    #out_tf = np.transpose(out_tf, (0, 3, 1, 2))
-    #print("TF:", out_tf)
-    #print("TF max:", out_tf.numpy().argmax())
+    print("TF:", out_tf)
+    print("TF max:", out_tf.numpy().argmax())
     return out_tf
 
 # (10,) -> (10,)
@@ -201,31 +157,7 @@ def conv2d(in_channels, out_channels, kernel_size, weight, bias, x):
         out[c, :, :] = bias[c] + s
     return out
 
-def batch_norm_2d(in_channels,
-        gamma, beta,
-        moving_mean, moving_variance,
-        x, eps, momentum):
-    assert len(x.shape) == 3
-    C, W, H = x.shape
-    assert gamma.shape == (C,)
-    assert beta.shape == (C,)
-    assert moving_mean.shape == (C,)
-    assert moving_variance.shape == (C,)
-    y = np.empty((in_channels, W, H), dtype=x.dtype)
-    for c in range(C):
-        #y[c,:,:] = (x[c,:,:] - np.mean(x[c,:,:])) / \
-        #        np.sqrt(np.var(x[c,:,:]) + eps)
-        y[c,:,:] = ((x[c,:,:] - moving_mean[c]) \
-                / np.sqrt(moving_variance[c] + eps)) * gamma[c] + beta[c]
-    return y
-
-def run_model_np(inp,
-        kernel1, bias1, kernel2, bias2, kernel3, bias3, kernel4, bias4,
-        batchnorm1_gamma, batchnorm1_beta,
-        batchnorm1_moving_mean, batchnorm1_moving_variance,
-        batchnorm2_gamma, batchnorm2_beta,
-        batchnorm2_moving_mean, batchnorm2_moving_variance,
-        dense_w, dense_b):
+def run_model_np(inp, kernel1, bias1, kernel2, bias2, dense_w, dense_b):
     print("Input shape:", inp.shape)
     assert inp.shape == (28, 28)
     inp_ = np.expand_dims(inp, 0)
@@ -233,50 +165,38 @@ def run_model_np(inp,
 
     # Conv2D
     # (C_out, C_in, H, W)
-    out = conv2d(1, 32, 5, kernel1, bias1, out)
+    out = conv2d(1, 32, 3, kernel1, bias1, out)
+
     # ReLU
     out = relu(out)
-    # (C_out, C_in, H, W)
-    out = conv2d(32, 32, 5, kernel2, bias2, out)
-    # ReLU
-    out = relu(out)
-    # BatchNorm2D
-    out = batch_norm_2d(32,
-            batchnorm1_gamma, batchnorm1_beta,
-            batchnorm1_moving_mean, batchnorm1_moving_variance,
-            out, eps=0.001, momentum=0.01)
+
     # MaxPool2D
     out = max_pool_2d(out)
 
     # Conv2D
     # (C_out, C_in, H, W)
-    out = conv2d(32, 64, 3, kernel3, bias3, out)
+    out = conv2d(32, 64, 3, kernel2, bias2, out)
+
     # ReLU
     out = relu(out)
-    # (C_out, C_in, H, W)
-    out = conv2d(64, 64, 3, kernel4, bias4, out)
-    # ReLU
-    out = relu(out)
-    # BatchNorm2D
-    out = batch_norm_2d(64,
-            batchnorm2_gamma, batchnorm2_beta,
-            batchnorm2_moving_mean, batchnorm2_moving_variance,
-            out, eps=0.001, momentum=0.01)
+
     # MaxPool2D
     out = max_pool_2d(out)
 
     # Flatten
-    out = np.reshape(out, (576,))
+    out = np.reshape(out, (1600,))
+
     # Linear
     # (N_out, C_in*H*W)
     out = np.dot(dense_w, out) + dense_b
+
     # Softmax
     out = softmax(out)
 
-    #print("Output shape:", out.shape)
+    print("Output shape:", out.shape)
     assert out.shape == (10,)
-    #print("NumPy:", out)
-    #print("NumPy max:", out.argmax())
+    print("NumPy:", out)
+    print("NumPy max:", out.argmax())
 
     return out
 
@@ -287,29 +207,13 @@ def main():
     print("    Done.")
 
     print("Loading MNIST model GGUF...")
-    g = GGUFReader("mnist-cnn-beautiful-model.gguf")
+    g = GGUFReader("mnist-cnn-model.gguf")
     kernel1 = gguf_to_array(g.tensors[0], "kernel1")
     bias1 = gguf_to_array(g.tensors[1], "bias1")
     kernel2 = gguf_to_array(g.tensors[2], "kernel2")
     bias2 = gguf_to_array(g.tensors[3], "bias2")
-    batchnorm1_gamma = gguf_to_array(g.tensors[4], "batchnorm1_gamma")
-    batchnorm1_beta = gguf_to_array(g.tensors[5], "batchnorm1_beta")
-    batchnorm1_moving_mean = gguf_to_array(g.tensors[6],
-            "batchnorm1_moving_mean")
-    batchnorm1_moving_variance = gguf_to_array(g.tensors[7],
-            "batchnorm1_moving_variance")
-    kernel3 = gguf_to_array(g.tensors[8], "kernel3")
-    bias3 = gguf_to_array(g.tensors[9], "bias3")
-    kernel4 = gguf_to_array(g.tensors[10], "kernel4")
-    bias4 = gguf_to_array(g.tensors[11], "bias4")
-    batchnorm2_gamma = gguf_to_array(g.tensors[12], "batchnorm2_gamma")
-    batchnorm2_beta = gguf_to_array(g.tensors[13], "batchnorm2_beta")
-    batchnorm2_moving_mean = gguf_to_array(g.tensors[14],
-            "batchnorm2_moving_mean")
-    batchnorm2_moving_variance = gguf_to_array(g.tensors[15],
-            "batchnorm2_moving_variance")
-    dense_w = gguf_to_array(g.tensors[16], "dense_w")
-    dense_b = gguf_to_array(g.tensors[17], "dense_b")
+    dense_w = gguf_to_array(g.tensors[4], "dense_w")
+    dense_b = gguf_to_array(g.tensors[5], "dense_b")
     print("    Done.")
 
     for iter in range(N_iter):
@@ -319,31 +223,18 @@ def main():
         draw_digit(inp)
         print("Reference value:", y_test[i])
 
-        #x = run_model_pt(inp,
-        #        kernel1, bias1, kernel2, bias2, kernel3, bias3, kernel4, bias4,
-        #        batchnorm1_moving_mean,
-        #            batchnorm1_moving_variance,
-        #        batchnorm2_gamma, batchnorm2_beta, batchnorm2_moving_mean,
-        #            batchnorm2_moving_variance,
-        #        dense_w, dense_b)
-        #infer_val = np.argmax(x)
-        #print("Inferred value:", infer_val)
-        #print("Digit probabilities:", x)
-        #print(x[0,:,:1,:1])
-
-        x = run_model_tf(inp)
+        x = run_model_pt(inp, kernel1, bias1, kernel2, bias2, dense_w, dense_b)
         infer_val = np.argmax(x)
-        print("TF Inferred value:", infer_val)
-        print("TF Digit probabilities:", x)
+        print("Inferred value:", infer_val)
+        print("Digit probabilities:", x)
 
-        #print("---------")
-        x = run_model_np(inp,
-                kernel1, bias1, kernel2, bias2, kernel3, bias3, kernel4, bias4,
-                batchnorm1_gamma, batchnorm1_beta,
-                batchnorm1_moving_mean, batchnorm1_moving_variance,
-                batchnorm2_gamma, batchnorm2_beta,
-                batchnorm2_moving_mean, batchnorm2_moving_variance,
-                dense_w, dense_b)
+        x = run_model_tf(inp, kernel1, bias1, kernel2, bias2, dense_w, dense_b)
+        infer_val = np.argmax(x)
+        print("Inferred value:", infer_val)
+        print("Digit probabilities:", x)
+
+        print("---------")
+        x = run_model_np(inp, kernel1, bias1, kernel2, bias2, dense_w, dense_b)
         infer_val = np.argmax(x)
         print("NumPy Inferred value:", infer_val)
         print("NumPy Digit probabilities:", x)
